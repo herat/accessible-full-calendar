626f376ce545a2460c0aa8332ea323f11c91974c
heratgandhi@gmail.com <heratgandhi@gmail.com@338eaf16-38bd-02a1-8f9d-59e459a8e10c>
2012-05-29 06:59:32 +0000

    DB connect

--- demos/json-events.php
+++ demos/json-events.php
@@ -2,27 +2,27 @@
 
 	$year = date('Y');
 	$month = date('m');
-
-	echo json_encode(array(
 	
-		array(
-			'id' => 111,
-			'title' => "Event1",
-			'start' => "$year-$month-10",
-			'url' => "http://yahoo.com/"
-		),
-		
-		array(
-			'id' => 222,
-			'title' => "Event2",
-			'start' => "$year-$month-20",
-			'end' => "$year-$month-22",
-			'url' => "http://yahoo.com/",
-			'color' => "rgb(0,128,128)",
-			'borderColor' => "rgb(0,0,0)",
-			'textColor' => "rgb(0,64,23)"
-		)
+	$con = mysql_connect("localhost","root","");
+	if (!$con)
+	{
+	  die('Could not connect: ' . mysql_error());
+	}
 	
-	));
+	mysql_select_db("test");
+	
+	$query = "SELECT * FROM `full-calendar-events`";
+	$result = mysql_query($query);
+	
+	$rows = array();
+	
+	while ($row = mysql_fetch_assoc($result)) 
+	{
+		array_push( $rows, $row );		
+	}
 
+	//mysql_free_result($result);
+	//mysql_close($db);
+	
+	echo json_encode( $rows );
 ?>
