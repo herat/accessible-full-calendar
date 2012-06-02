0d92d5584947627099cdeb84c2d56e83faca062b
heratgandhi@gmail.com <heratgandhi@gmail.com@338eaf16-38bd-02a1-8f9d-59e459a8e10c>
2012-06-02 03:21:15 +0000

    Comments in PHP

--- trunk/demos/update.php
+++ trunk/demos/update.php
@@ -1,42 +1,52 @@
 <?php
-
-$iddd = $_GET["id"];
-$newstartd = $_GET["start"];
-$newend = $_GET["end"];
-$newtitle = $_GET["title"];
-$alld = $_GET["allday"];
-$command = $_GET["cmd"];
-
-$con = mysql_connect("localhost","root","root");
-if (!$con)
-{
-  die('Could not connect: ' . mysql_error());
-}
-
-mysql_select_db("test");
-
-if( strcmp($command,"drag") == 0 )
-{
-	$query = "UPDATE `full-calendar-events` SET start = '".$newstartd."', end = '".$newend."' WHERE id=".$iddd;
-	mysql_query( $query, $con );
-}
-else if( strcmp($command,"create") == 0 )
-{
-	$query = "INSERT INTO `full-calendar-events` (title,start,end,allDay) values ('".$newtitle."','".$newstartd."','".$newend."','".$alld."')" ;
-	mysql_query( $query, $con );
-	$query = "SELECT MAX(id) from `full-calendar-events`";
-	$resultno = mysql_query( $query, $con );
-	$idno=mysql_fetch_row($resultno);  
-	echo $idno[0];	
-}
-else if( strcmp($command,"delete") == 0 )
-{
-	$query = "DELETE FROM `full-calendar-events` WHERE id=".$iddd;
-	mysql_query( $query, $con );
-}
-else if( strcmp($command,"update") == 0 )
-{
-	$query = "UPDATE `full-calendar-events` SET title = '".$newtitle."' , start = '".$newstartd."', end = '".$newend."' WHERE id=".$iddd;
-	mysql_query( $query, $con );
-}
+	//Retrieve all the parameters from request.
+	$iddd = $_GET["id"];
+	$newstartd = $_GET["start"];
+	$newend = $_GET["end"];
+	$newtitle = $_GET["title"];
+	$alld = $_GET["allday"];
+	$command = $_GET["cmd"];
+	
+	//Connect to database.
+	$con = mysql_connect("localhost","root","root");
+	if (!$con)
+	{
+	  die('Could not connect: ' . mysql_error());
+	}
+	
+	//Select database "test".
+	mysql_select_db("test");
+	
+	//Use SQL Query according to the situation. 
+	if( strcmp($command,"drag") == 0 )
+	{
+		//Event is dragged, so update dates.
+		$query = "UPDATE `full-calendar-events` SET start = '".$newstartd.
+		"', end = '".$newend."' WHERE id=".$iddd;
+		mysql_query( $query, $con );
+	}
+	else if( strcmp($command,"create") == 0 )
+	{
+		//New event is created, first insert the record in the table and then return id to javascript.
+		$query = "INSERT INTO `full-calendar-events` (title,start,end,allDay) values".
+		" ('".$newtitle."','".$newstartd."','".$newend."','".$alld."')" ;
+		mysql_query( $query, $con );
+		$query = "SELECT MAX(id) from `full-calendar-events`";
+		$resultno = mysql_query( $query, $con );
+		$idno=mysql_fetch_row($resultno);  
+		echo $idno[0];	
+	}
+	else if( strcmp($command,"delete") == 0 )
+	{
+		//Delete the event.
+		$query = "DELETE FROM `full-calendar-events` WHERE id=".$iddd;
+		mysql_query( $query, $con );
+	}
+	else if( strcmp($command,"update") == 0 )
+	{
+		//User clicked on the event. The dates and title may be changed. So update the database record.
+		$query = "UPDATE `full-calendar-events` SET title = '".$newtitle."' , start = '".
+		$newstartd."', end = '".$newend."' WHERE id=".$iddd;
+		mysql_query( $query, $con );
+	}
 ?>
